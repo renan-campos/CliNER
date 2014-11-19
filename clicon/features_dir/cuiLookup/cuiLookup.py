c8d025e492377b21e4de33cbb4abb1478e9a5c59
ktwaco <kdubya12345@gmail.com>
2014-11-18 19:02:34 -0500

    added a cui lookup script

---
+++ clicon/features_dir/cuiLookup/cuiLookup.py
@@ -0,0 +1,100 @@
+import subprocess
+import re
+
+from sets import Set
+
+def getConceptId(phrase):
+    """
+    performs a cui lookup on a phrase using metamap java api.
+    """
+
+    # TODO: change this for when put into cliner
+    metaMapApiJarPath  = ":/src/javaapi/dist/MetaMapApi.jar"
+    prologBeansJarPath = ":/src/javaapi/dist/prologbeans.jar"
+
+    # jar paths used for -cp. used to linked dependencies to program
+    cpArgs =  ".{0}{1}".format(metaMapApiJarPath,
+                              prologBeansJarPath)
+    # the compiled java program to execute
+    progArg = "gov.nih.nlm.nls.metamap.cuiLookup"
+
+    # get resulting output of lookup
+    stdout = subprocess.check_output(["java", "-cp", cpArgs, progArg, phrase])
+
+    # dictionary
+    result = extractConceptIdsFromStdout(stdout)
+
+    return result
+
+def extractConceptIdsFromStdout(stdout):
+    """
+    processes output produced from java program cuiLookup
+
+    and returns a dictionary of phrases mapped to a set of concept id's
+
+    a phrase will be mapped to None if there are no concept id's
+    """
+
+    results = {}
+
+    for line in stdout.split('\n'):
+
+        if lineIsPhrase(line):
+
+            phrase =  extractPhrase(line)
+
+            # if line is a phrase output then create new if in not already existing
+            if phrase not in results:
+
+                results["concept_ids"] = None
+
+                results["text"] = phrase
+
+        if lineIsConceptId(line):
+        
+            cui = extractCui(line)
+
+            if cui is None:
+                
+                # if there is no concept id then do nothing.
+                pass
+
+            elif results["concept_ids"] is None:
+
+                # if there is a concept id but nothing is already assigned to the phrase then assign a list.
+                results["concept_ids"] = Set()
+
+                results["concept_ids"].add(cui)
+
+            else:
+       
+                # if a list is already assigned then append to concept id list.
+                results["concept_ids"].add(cui)
+
+    return results
+
+def lineIsPhrase(lineFromStdout):
+    return ("phrase: " in lineFromStdout)
+
+def lineIsConceptId(lineFromStdout):
+    return ("concept id: " in lineFromStdout)
+
+def extractPhrase(lineFromStdout):
+    return re.sub("phrase: ", '', lineFromStdout)
+
+def extractCui(lineFromStdout):
+    cui = re.sub("concept id: ", '', lineFromStdout)
+
+    if cui == "None":
+        cui = None
+
+    return cui
+
+if __name__ == "__main__":
+    
+    print "Input phrase: "
+    phrase = input()
+
+    print getConceptId(phrase)
+    #print cuiLookup("blood")
+    #print cuiLookup("the man went down to georgia")
